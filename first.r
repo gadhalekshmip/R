7a3acc251bce49cd3f3bb2723240aeb33241f7df
gadhalekshmip <gadhalekshmip@gmail.com>
2023-05-22 16:35:07 +0530

    update

--- first.r
+++ first.r
@@ -1,7 +1,15 @@
-# basics of R 
-# subtraction of two numbers
-a <- 1
-b <- 6
-print(a-b)
+#Datatypes
 
-#
+a <- FALSE
+print(class(a))
+
+a <- 2L
+print(class(a))
+
+a <-charToRaw("hello world")
+print(class(a))
+
+#Output
+[1] "logical"
+[1] "integer"
+[1] "raw"
